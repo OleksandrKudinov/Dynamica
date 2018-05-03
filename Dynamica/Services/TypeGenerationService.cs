38334d749057f97b47cbbc0fe9368d08c3a4b168
oleksandr.kudinov <oleksandr.kudinov.1993@gmail.com>
2018-05-03 19:47:26 +0300

    created TypeGenerationservice

---
+++ Dynamica/Services/TypeGenerationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dynamica.Services
+{
+    public sealed class TypeGenerationService
+    {
+        public TypeGenerationService() //add custom generated type provider (loader)
+        {
+            var supportedTypes = new[]{
+                typeof(string),
+                typeof(int),
+                typeof(DateTime),
+                typeof(bool),
+                typeof(object),
+                typeof(Array)
+                // TODO add custom types
+            };
+
+            _supportedPropertyTypes = supportedTypes.ToDictionary(type => type.Name, type => type);
+        }
+
+        public Type GenerateTypeFromPropertyInfo(IEnumerable<KeyValuePair<string, Type>> properties)
+        {
+            // TODO 
+            throw new NotImplementedException();
+        }
+
+        public Type ResolveTypeByName(string typeName)
+        {
+            if (!_supportedPropertyTypes.ContainsKey(typeName))
+                throw new Exception($"Unsupported property type : {typeName}");
+
+            return _supportedPropertyTypes[typeName];
+        }
+
+        private readonly Dictionary<string, Type> _supportedPropertyTypes;
+    }
+}
