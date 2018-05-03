f60f2e1d4badef5c3a84ab4240c5135412fa6954
Oleksandr Kudinov <Oleksandr.Kudinov@eleks.com>
2018-05-03 19:22:13 +0300

    removed default ValuesController, added ItemsService for reading/writing objects to type-named json files

---
+++ Dynamica/Services/ItemsService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dynamica
+{
+    internal sealed class ItemsService
+    {
+        public void SaveItems(IEnumerable<object> items, Type type = null)
+        {
+            if (!items.Any())
+                return;
+
+            type = type ?? items.First().GetType();
+            JArray arr = JArray.FromObject(items);
+
+            string filename = $"{type.Name}.json";
+            System.IO.File.WriteAllText(filename, arr.ToString());
+        }
+
+        public IEnumerable<object> GetItems(Type type)
+        {
+            string filename = $"{type.Name}.json";
+
+            if (!System.IO.File.Exists(filename))
+                throw new Exception($"file {filename} does not exist");
+
+            var itemArrayStr = System.IO.File.ReadAllText(filename);
+            var items = JArray.Parse(itemArrayStr).Children().Select(x => x.ToObject(type));
+            return items;
+        }
+    }
+}
